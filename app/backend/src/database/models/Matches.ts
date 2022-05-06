a250c19d015c64c3cd8b4f4eb5ac86cbb15b0541
Thales <thalessilva.tv@gmail.com>
2022-05-06 12:05:46 -0300

    feat: create matches, teams and users models

---
+++ app/backend/src/database/models/Matches.ts
@@ -0,0 +1,57 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+import Team from './Teams';
+
+export default class Match extends Model {
+  public id: number;
+
+  public homeTeam: number;
+
+  public homeTeamGoals: number;
+
+  public awayTeam: number;
+
+  public awayTeamsGoals: number;
+
+  public inProgress: boolean;
+}
+
+Match.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  homeTeam: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+Team.hasMany(Match, { foreignKey: 'homeTeam', as: 'homeMatches' });
+Team.hasMany(Match, { foreignKey: 'awayTeam', as: 'awayMatches' });
+
+Match.belongsTo(Team, { foreignKey: 'homeTeam', as: 'teamHome' });
+Match.belongsTo(Team, { foreignKey: 'awayTeam', as: 'teamAway' });
--- app/backend/src/database/models/index.ts
+++ app/backend/src/database/models/index.ts
@@ -2,3 +2,7 @@ import { Sequelize } from 'sequelize';
 import * as config from '../config/database';
 
 export default new Sequelize(config);
+
+export { default as Teams } from './Teams';
+export { default as Matches } from './Matches';
+export { default as Users } from './Users';
