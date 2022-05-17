4eba5adea80ebd7871760192024abc8bf64a361b
Thales <thalessilva.tv@gmail.com>
2022-05-17 14:34:06 -0300

    feat: export route leaderboard

--- app/backend/src/router/index.ts
+++ app/backend/src/router/index.ts
@@ -1,3 +1,4 @@
 export { default as loginRouter } from './login.router';
 export { default as teamsRouter } from './teams.router';
 export { default as matchesRouter } from './matches.router';
+export { default as leaderboardRouter } from './leaderboard.router';
